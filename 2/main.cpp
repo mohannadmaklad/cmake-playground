d42fef7443d9129bd49db567a61ce6e56ef22c55
mohannadmaklad <mshm.maklad@gmail.com>
2022-08-01 20:04:48 +0200

    Adding batch files

---
+++ 2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	cout << "hello there" << "\n";
+	cout << add(1,2) << "\n";
+}
---
+++ 3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <add.h>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "hello there" << "\n";
+	cout << add(1,2) << "\n";
+}
